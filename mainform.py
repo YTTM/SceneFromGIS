bbcf43abc26fae7da4eb4be9fc9dc8029715a3d8
YTTM <yttrium@14egaming.com>
2023-05-08 18:49:10 +0200

    update gui

--- mainform.py
+++ mainform.py
@@ -67,15 +67,6 @@ class Ui_MainWindow(object):
         self.gridLayout_properties.setObjectName("gridLayout_properties")
         self.comboBox_1 = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox_1.setObjectName("comboBox_1")
-        self.comboBox_1.addItem("")
-        self.comboBox_1.addItem("")
-        self.comboBox_1.addItem("")
-        self.comboBox_1.addItem("")
-        self.comboBox_1.addItem("")
-        self.comboBox_1.addItem("")
-        self.comboBox_1.addItem("")
-        self.comboBox_1.addItem("")
-        self.comboBox_1.addItem("")
         self.gridLayout_properties.addWidget(self.comboBox_1, 0, 1, 1, 1)
         self.label_id_1 = QtWidgets.QLabel(self.centralwidget)
         self.label_id_1.setObjectName("label_id_1")
@@ -141,11 +132,12 @@ class Ui_MainWindow(object):
         self.pushButton_remove_layer.clicked.connect(MainWindow.event_pushbutton_remove_layer_clicked) # type: ignore
         self.pushButton_gen.clicked.connect(MainWindow.event_pushbutton_gen_clicked) # type: ignore
         self.pushButton_exp.clicked.connect(MainWindow.event_pushbutton_exp_clicked) # type: ignore
-        self.listWidget_input.currentRowChanged['int'].connect(MainWindow.event_listwidget_input_currentRowChanged) # type: ignore
+        self.listWidget_input.currentRowChanged['int'].connect(MainWindow.event_listwidget_input_currentrowchanged) # type: ignore
         self.actionNew.triggered.connect(MainWindow.event_action_new) # type: ignore
         self.actionOpen.triggered.connect(MainWindow.event_action_open) # type: ignore
         self.actionSave.triggered.connect(MainWindow.event_action_save) # type: ignore
         self.actionAdd.triggered.connect(MainWindow.event_action_add) # type: ignore
+        self.comboBox_1.activated['int'].connect(MainWindow.event_comboBox_1_activated) # type: ignore
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         MainWindow.setTabOrder(self.listWidget_input, self.pushButton_remove_layer)
         MainWindow.setTabOrder(self.pushButton_remove_layer, self.comboBox_1)
@@ -168,15 +160,6 @@ class Ui_MainWindow(object):
         self.pushButton_gen.setText(_translate("MainWindow", "Generate"))
         self.pushButton_exp.setText(_translate("MainWindow", "Export"))
         self.pushButton_remove_layer.setText(_translate("MainWindow", "Remove layer"))
-        self.comboBox_1.setItemText(0, _translate("MainWindow", "heightmap"))
-        self.comboBox_1.setItemText(1, _translate("MainWindow", "path (line)"))
-        self.comboBox_1.setItemText(2, _translate("MainWindow", "building (line)"))
-        self.comboBox_1.setItemText(3, _translate("MainWindow", "building (polygon)"))
-        self.comboBox_1.setItemText(4, _translate("MainWindow", "forest (line)"))
-        self.comboBox_1.setItemText(5, _translate("MainWindow", "forest (polygon)"))
-        self.comboBox_1.setItemText(6, _translate("MainWindow", "forest (point)"))
-        self.comboBox_1.setItemText(7, _translate("MainWindow", "water (line)"))
-        self.comboBox_1.setItemText(8, _translate("MainWindow", "water (polygon)"))
         self.label_id_1.setText(_translate("MainWindow", "Type"))
         self.label_uint_2.setText(_translate("MainWindow", "2"))
         self.label_ushort_3.setText(_translate("MainWindow", "3"))
