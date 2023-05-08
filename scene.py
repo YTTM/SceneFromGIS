02a1b7166543f740a89916be9bcf9e6504ae1085
YTTM <yttrium@14egaming.com>
2023-05-08 19:46:09 +0200

    add more data to scene layers

--- scene.py
+++ scene.py
@@ -7,14 +7,29 @@ layer_types = ['heightmap', 'path (line)',
                'water (polygon)']
 layer_symbol = ['🏞️', '🛣️', '🏠', '🧱', '🌿', '🌳', '🌲', '💧', '🌊']
 
+import raster
 
 class Scene:
     def __init__(self):
+        # layer format : [filename, layer_type, layer_valid, layer_view, layer_data]
         self.layers = []
         return
 
     def add_layer(self, filename, layer_type):
-        self.layers.append([filename, layer_type])
+        self.layers.append([filename, layer_type, None, None, None])
+
+        if layer_type == 0:
+            data, (bounds) = raster.read(filename)
+            x_min, x_max, y_min, y_max, z_min, z_max = bounds
+            view = raster.view(data)
+
+            self.layers[-1][2] = True
+            self.layers[-1][3] = view
+            self.layers[-1][4] = (data, bounds)
+        '''
+        else:
+            vector.read()
+        '''
 
     def remove_layer(self, i):
         del self.layers[i]
